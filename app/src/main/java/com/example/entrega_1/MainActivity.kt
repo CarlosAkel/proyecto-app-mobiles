81a1489bde15cacbf84687622976fa533f847197
francisco-delamaza <fjdelamaza@miuandes.cl>
2021-04-18 18:23:43 -0400

    Crado el main activity login

--- app/src/main/java/com/example/entrega_1/MainActivity.kt
+++ app/src/main/java/com/example/entrega_1/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.entrega_1
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val boton = findViewById<Button>(R.id.button_login)
+        boton.setOnClickListener {
+            val intent = Intent(this, card::class.java)
+            startActivity(intent)
+
+        }
     }
 }
